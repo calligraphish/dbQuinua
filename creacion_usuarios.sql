429759b492fc6eb69db523ab6889afaaf3814090
avalon1805 <escamilla.marcelo@yahoo.com>
2017-05-29 17:26:32 -0500

    agregados los permisos para P.As al admon

--- creacion_usuarios.sql
+++ creacion_usuarios.sql
@@ -15,4 +15,11 @@ grant select on db_1.vw_clientes to 'admon'@'localhost';
 grant select on db_1.vw_rutarepartidor to 'admon'@'localhost';
 grant select on db_1.vw_ventas_admin to 'admon'@'localhost';
 grant select on db_1.vw_inventariocl to 'admon'@'localhost';
+grant select on db_1.establecimiento_v to 'admon'@'localhost';
 
+grant execute on procedure db_1.sp_cambiar_repartidor to 'admon'@'localhost';
+grant execute on procedure db_1.sp_compracl to 'admon'@'localhost';
+grant execute on procedure db_1.comprapr to 'admon'@'localhost';
+grant execute on procedure db_1.sp_crearuta to 'admon'@'localhost';
+grant execute on procedure db_1.sp_registrocl to 'admon'@'localhost';
+grant execute on procedure db_1.sp_rutaventa to 'admon'@'localhost';
